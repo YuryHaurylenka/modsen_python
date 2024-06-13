01df3235f6f85527a132f2f2097a11272c3ca194
GavrilenkoYura <gshshaaha@gmail.com>
2024-06-13 20:47:29 +0300

    add funcs to find and display duplicates and refactor calculate_image_hashes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def load_images_from_folder(folder: str) -> dict:
             img_path = os.path.join(folder, filename)
             try:
                 img = Image.open(img_path)
-                images[img_path]
+                images[img_path] = img
             except (IOError, SyntaxError) as e:
                 print(f"Error loading image {img_path}: {e}")
     return images
@@ -34,4 +34,22 @@ def calculate_image_hashes(images: dict) -> dict:
                 hashes[img_hash] = [path]
         except Exception as e:
             print(f"Error calculating hash for {path}: {e}")
-    return hash()
+    return hashes
+
+
+def find_duplicates(hashes: dict) -> dict:
+    """
+    Find duplicate images relying on hashes sum.
+    """
+    duplicates = {k: v for k, v in hashes.items() if len(v) > 1}
+    return duplicates
+
+def display_duplicates(duplicates: dict) -> None:
+    """
+    Display duplicate images in console.
+    """
+    for img_hash, paths in duplicates.items():
+        print(f"Hash: {img_hash}")
+        for path in paths:
+            print(f"\t{path}")
+        print()
