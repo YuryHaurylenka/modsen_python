d0023ad1e550a06f98c77905388cdc03fa5f54da
GavrilenkoYura <gshshaaha@gmail.com>
2024-06-19 21:04:38 +0300

    refactoring

--- test_assignment/test_main.py
+++ test_assignment/test_main.py
@@ -1,7 +1,7 @@
 import os
 
 import pytest
-from PIL import Image
+from PIL import Image, ImageDraw
 from imagehash import ImageHash
 
 from main import calculate_image_hashes, find_duplicates, load_images_from_folder, save_result
@@ -9,7 +9,6 @@ from main import calculate_image_hashes, find_duplicates, load_images_from_folde
 TEST_FOLDER = "test_images"
 TEST_IMAGE_1 = os.path.join(TEST_FOLDER, "image1.jpg")
 TEST_IMAGE_2 = os.path.join(TEST_FOLDER, "image2.png")
-TEST_IMAGE_UNSUPPORTED = os.path.join(TEST_FOLDER, "unsupported.txt")
 TEST_IMAGE_CORRUPTED = os.path.join(TEST_FOLDER, "corrupted.jpg")
 TEST_IMAGE_DUPLICATE = os.path.join(TEST_FOLDER, "duplicate.jpg")
 
@@ -21,14 +20,15 @@ def setup_test_folder():
     """
     os.makedirs(TEST_FOLDER, exist_ok=True)
 
-    Image.new('RGB', (100, 100)).save(TEST_IMAGE_1)
-    Image.new('RGB', (100, 100)).save(TEST_IMAGE_2)
-    Image.new('RGB', (100, 100)).save(TEST_IMAGE_DUPLICATE)
+    Image.new('RGB', (100, 100), 'red').save(TEST_IMAGE_1)
+    image2 = Image.new('RGB', (100, 100), 'green')
+    draw_image2 = ImageDraw.Draw(image2)
+    draw_image2.text((10,40), 'This is image 2', fill='white')
+    image2.save(TEST_IMAGE_2)
+    Image.new('RGB', (100, 100) , 'red').save(TEST_IMAGE_DUPLICATE)
 
-    with open(TEST_IMAGE_UNSUPPORTED, "w") as f:
-        f.write("This is not an image")
     yield
-    for file in [TEST_IMAGE_1, TEST_IMAGE_2, TEST_IMAGE_UNSUPPORTED, TEST_IMAGE_CORRUPTED, TEST_IMAGE_DUPLICATE]:
+    for file in [TEST_IMAGE_1, TEST_IMAGE_2, TEST_IMAGE_CORRUPTED, TEST_IMAGE_DUPLICATE]:
         if os.path.exists(file):
             os.remove(file)
     os.rmdir(TEST_FOLDER)
@@ -36,46 +36,44 @@ def setup_test_folder():
 
 def test_load_images_from_folder():
 
-    with pytest.raises(ValueError) as excinfo:
-        images = load_images_from_folder(TEST_FOLDER, supported_formats=['.jpg', '.png'])
-        assert str(excinfo.value) == "Unsupported image format: .txt"
-        assert len(images) == 3
+    images = load_images_from_folder(TEST_FOLDER, supported_formats=['.jpg', '.png'])
+    assert len(images) == 3
 
 
 def test_calculate_image_hashes():
-    with pytest.raises(ValueError):
-        images = load_images_from_folder(TEST_FOLDER)
 
-        hashes = calculate_image_hashes(images)
-        assert isinstance(hashes, dict)
-        assert len(hashes) == 2
-        for img_hash, paths in hashes.items():
-            assert isinstance(img_hash, ImageHash)
-            assert isinstance(paths, list)
-            assert all(isinstance(path, str) for path in paths)
+    images = load_images_from_folder(TEST_FOLDER)
 
-    with pytest.raises(Exception):
-        calculate_image_hashes({"error_image.jpg": Image.open(TEST_IMAGE_CORRUPTED)})
+    hashes = calculate_image_hashes(images)
+    assert isinstance(hashes, dict)
+    assert len(hashes) == 2
+    for img_hash, paths in hashes.items():
+        assert isinstance(img_hash, ImageHash)
+        assert isinstance(paths, list)
+        assert all(isinstance(path, str) for path in paths)
+
+
+with pytest.raises(Exception):
+    calculate_image_hashes({"error_image.jpg": Image.open(TEST_IMAGE_CORRUPTED)})
 
 
 def test_find_duplicates():
-    with pytest.raises(ValueError):
-        images = load_images_from_folder(TEST_FOLDER)
-        hashes = calculate_image_hashes(images)
-        duplicates = find_duplicates(hashes)
 
-        assert isinstance(duplicates, dict)
-        assert len(duplicates) == 1
-        assert len(duplicates[list(duplicates.keys())[0]]) == 2
+    images = load_images_from_folder(TEST_FOLDER)
+    hashes = calculate_image_hashes(images)
+    duplicates = find_duplicates(hashes)
+
+    assert len(duplicates) == 1
+    assert len(duplicates[list(duplicates.keys())[0]]) == 2
 
 
 def test_save_result():
-    with pytest.raises(ValueError):
-        images = load_images_from_folder(TEST_FOLDER)
-        hashes = calculate_image_hashes(images)
-        duplicates = find_duplicates(hashes)
 
-        save_result(duplicates, output_file="test_duplicates.csv")
-        assert os.path.exists("test_duplicates.csv")
+    images = load_images_from_folder(TEST_FOLDER)
+    hashes = calculate_image_hashes(images)
+    duplicates = find_duplicates(hashes)
 
-        os.remove("test_duplicates.csv")
+    save_result(duplicates, output_file="test_duplicates.csv")
+    assert os.path.exists("test_duplicates.csv")
+
+    os.remove("test_duplicates.csv")
