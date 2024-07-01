2f30493880887dffbd3a0a63f2210d2d6853cd56
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-01 11:35:42 +0300

    add parallel processing into the image loading

--- test_assignment/image_loader.py
+++ test_assignment/image_loader.py
@@ -1,28 +1,38 @@
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Dict, List
 
 from PIL import Image, UnidentifiedImageError
 
 
+def load_image(img_path: str) -> (str, Image.Image):
+    """
+    Load one image from folder.
+    """
+    try:
+        img = Image.open(img_path)
+        return img_path, img
+    except (IOError, UnidentifiedImageError) as e:
+        print(f"Error loading image {img_path}: {e}")
+        return img_path, None
+
+
 def load_images_from_folder(folder: str,
                             supported_formats: List[str] = None) -> Dict[str, Image.Image]:
     """
-    Load images from the folder.
+    Load all mages from the folder.
     """
     if supported_formats is None:
         supported_formats = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
 
     images: Dict[str, Image.Image] = {}
+    file_paths = [os.path.join(folder, filename) for filename in os.listdir(folder)
+                  if os.path.splitext(filename)[1].lower() in supported_formats]
 
-    for filename in os.listdir(folder):
-        file_ext = os.path.splitext(filename)[1].lower()
-        if file_ext not in supported_formats:
-            print(f"Unsupported image format: {file_ext}")
-            continue
-        img_path = os.path.join(folder, filename)
-        try:
-            img = Image.open(img_path)
-            images[img_path] = img
-        except (IOError, UnidentifiedImageError) as e:
-            print(f"Error loading image {img_path}: {e}")
+    with ThreadPoolExecutor() as executor:
+        future_to_path = {executor.submit(load_image, path): path for path in file_paths}
+        for future in as_completed(future_to_path):
+            path, img = future.result()
+            if img is not None:
+                images[path] = img
     return images
